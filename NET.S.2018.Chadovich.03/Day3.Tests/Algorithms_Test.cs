20b2cf3650461d98ca3f29fb1719e3c959e1f58f
Valery Čadovič <valerachad03@gmail.com>
2018-06-29 18:00:05 +0300

    1st task completed

---
+++ NET.S.2018.Chadovich.03/Day3.Tests/Algorithms_Test.cs
@@ -0,0 +1,26 @@
+﻿namespace Day3.Tests
+{
+    using System;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class Algorithms_Test
+    {
+        [TestCase(1, 5, 0.000_001, 1)]
+        [TestCase(8, 3, 0.0001, 2)]
+        [TestCase(0.001, 3, 0.0001, 0.1)]
+        [TestCase(0.041_006_25, 4, 0.0001, 0.45)]
+        [TestCase(0.027_9936, 7, 0.0001, 0.6)]
+        [TestCase(-0.008, 3, 0.1, -0.2)]
+        [TestCase(0.004_241_979, 9, 0.000_000_01, 0.545)]
+        [TestCase(0.0081, 4, 0.1,  0.3)]
+        public void Can_Root_Precision(double number, int power, double precision, double almostResult) =>
+            Assert.AreEqual(Algorithms.RealRoot(number, power, precision), almostResult, precision);
+
+        [TestCase(-0.01, 2, 0.0001)]
+        [TestCase(0.001, -2, 0.0001)]
+        [TestCase(0.01, 2, -1)]
+        public void RealRoot_Throws_ArgumentOutOfRangeException(double number, int power, double precision) =>
+            Assert.Throws<ArgumentOutOfRangeException>(() => Algorithms.RealRoot(number, power, precision));
+    }
+}
---
+++ NET.S.2018.Chadovich.03/Day3/Algorithms.cs
@@ -0,0 +1,52 @@
+﻿namespace Day3
+{
+    using System;
+
+    /// <summary>
+    /// Includes a set of basic algorithms
+    /// </summary>
+    public static class Algorithms
+    {
+        /// <summary>
+        /// Finds a real root of a real number
+        /// </summary>
+        /// <param name="number">preset number</param>
+        /// <param name="power">root degree</param>
+        /// <param name="precision">accuracy of measurement</param>
+        /// <returns>nth root as real number</returns>
+        public static double RealRoot(double number, int power, double precision)
+        {
+            if (precision >= 1 || precision <= 0)
+            {
+                throw new ArgumentOutOfRangeException($"{nameof(precision)} must be in range (0, 1)");
+            }
+            
+            if (power == 1)
+            {
+                return number;
+            }
+
+            if (power <= 0)
+            {
+                throw new ArgumentOutOfRangeException($"{nameof(power)} must be positive to work with real numbers only");
+            }
+
+            if (power % 2 == 0 && number < 0)
+            {
+                throw new ArgumentOutOfRangeException($"{nameof(number)} will be non-real through the current operation");
+            }
+
+            double current = 1;
+            double next = (((power - 1) * current) + (number / Math.Pow(current, power - 1))) / power;
+            
+            do
+            {
+                current = next;
+                next = (((power - 1) * current) + (number / Math.Pow(current, power - 1))) / power;
+            }
+            while (Math.Abs(next - current) > precision);
+
+            return next;
+        }
+    }
+}
