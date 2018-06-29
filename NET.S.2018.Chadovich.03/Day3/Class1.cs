13da6a276bbd406f64ccc996c200e7f13cd1d05c
Valery Čadovič <valerachad03@gmail.com>
2018-06-29 12:12:28 +0300

    test github from vs

---
+++ NET.S.2018.Chadovich.03/Day3/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    public static class Algorithms
+    {
+        public static double Sqrt(double x) { return default(double); }
+    }
+}
