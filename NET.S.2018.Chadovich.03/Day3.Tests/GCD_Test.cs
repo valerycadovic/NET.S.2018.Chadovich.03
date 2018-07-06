d2e1a25fc9a0d2bd4ccfb4b1165aa124cf73adbe
Valery Čadovič <valerachad03@gmail.com>
2018-07-06 22:14:01 +0300

    tests wete polny otstoj

--- NET.S.2018.Chadovich.03/Day3.Tests/GCD_Test.cs
+++ NET.S.2018.Chadovich.03/Day3.Tests/GCD_Test.cs
@@ -13,21 +13,19 @@ namespace Day3.Tests
     {
         #region Euclid Tests
         [TestCase(0x10_000_000, 0x1_000_000, 0x100_000, 0x10_000, 0x1000, 0x100, 0x10, ExpectedResult = 16)]
+        [TestCase(0, 2, ExpectedResult = 2)]
+        [TestCase(0, 0, ExpectedResult = 0)]
+        [TestCase(-3, 4, ExpectedResult = 1)]
+        [TestCase(-6, -7, ExpectedResult = 1)]
+        [TestCase(16, 20, ExpectedResult = 4)]
+        [TestCase(16, 20, 28, ExpectedResult = 4)]
         public int Can_Euclid_Of_N_Numbers(params int[] numbers)
         {
             var result = GCD.Euclid(out long delay, numbers);
             Debug.WriteLine($"{nameof(GCD.Euclid)}: {delay}");
             return result;
         }
-
-        [TestCase(16, 20, ExpectedResult = 4)]
-        public int Can_Euclid_Of_2_Numbers(int number1, int number2) =>
-            GCD.Euclid(number1, number2);
-
-        [TestCase(16, 20, 28, ExpectedResult = 4)]
-        public int Can_Euclid_Of_3_Numbers(int number1, int number2, int number3) =>
-            GCD.Euclid(number1, number2, number3);
-
+        
         [TestCase(2)]
         [TestCase()]
         public void Euclid_Throws_ArgumentException(params int[] numbers) =>
@@ -36,21 +34,19 @@ namespace Day3.Tests
 
         #region Stein Tests
         [TestCase(0x10_000_000, 0x1_000_000, 0x100_000, 0x10_000, 0x1000, 0x100, 0x10, ExpectedResult = 16)]
+        [TestCase(0, 2, ExpectedResult = 2)]
+        [TestCase(0, 0, ExpectedResult = 0)]
+        [TestCase(-3, 4, ExpectedResult = 1)]
+        [TestCase(-6, -7, ExpectedResult = 1)]
+        [TestCase(16, 20, ExpectedResult = 4)]
+        [TestCase(16, 20, 28, ExpectedResult = 4)]
         public int Can_Stein_Of_N_Numbers(params int[] numbers)
         {
             var result = GCD.Stein(out long delay, numbers);
             Debug.WriteLine($"{nameof(GCD.Stein)}: {delay}");
             return result;
         }
-
-        [TestCase(16, 20, ExpectedResult = 4)]
-        public int Can_Stein_Of_2_Numbers(int number1, int number2) =>
-                GCD.Stein(number1, number2);
-
-        [TestCase(16, 20, 28, ExpectedResult = 4)]
-        public int Can_Stein_Of_3_Numbers(int number1, int number2, int number3) =>
-            GCD.Stein(number1, number2, number3);
-
+        
         [TestCase(2)]
         [TestCase()]
         public void Stein_Throws_ArgumentException(params int[] numbers) =>
